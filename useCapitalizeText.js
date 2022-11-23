e46acfeff69f4dff85d1384fade1f1c7710aa8e1
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2022-11-23 10:59:25 +0700

    Create useCapitalizeText.js

---
+++ useCapitalizeText.js
@@ -0,0 +1,23 @@
+const useCapitalizeText = () => {
+
+  const capitalizeText = (text) => {
+    
+    const words = text.split(' ') //split text of sentence into words
+
+    //capitalize first letter of every word
+    if (words.length > 1) {
+      for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1)
+      }
+
+      return words.join(' ') //join all words back into a sentence
+    } else {
+      //just capitalize 1st letter of a single word
+      return text.charAt(0).toUpperCase() + text.slice(1)
+    }
+  }
+  
+  return { capitalizeText }
+}
+
+export default useCapitalizeText
