70dabdaded31734c3767c61f17a677eda0c8031a
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2022-12-22 21:08:46 +0700

    Update JavascriptArrayHigherOrder.js

--- JavascriptArrayHigherOrder.js
+++ JavascriptArrayHigherOrder.js
@@ -17,3 +17,53 @@ movies = [{
 
 filters = ['Romance', 'Drama']
 console.log(movies.filter(x => x.genres.some(g => filters.includes(g))))
+
+//********************************************************************************************************
+//Group an array of objects by keys
+const cars = [
+    {
+        'make': 'audi',
+        'model': 'r8',
+        'year': '2012'
+    }, {
+        'make': 'audi',
+        'model': 'rs5',
+        'year': '2013'
+    }, {
+        'make': 'ford',
+        'model': 'mustang',
+        'year': '2012'
+    }, {
+        'make': 'ford',
+        'model': 'fusion',
+        'year': '2015'
+    }
+];
+
+const result = cars.reduce(function (r, a) {
+        r[a.make] = r[a.make] || [];
+        r[a.make].push(a);
+        return r;
+    }, {});
+//now result will be grouped as below
+const cars_result = {
+    'audi': [
+        {
+            'model': 'r8',
+            'year': '2012'
+        }, {
+            'model': 'rs5',
+            'year': '2013'
+        },
+    ],
+
+    'ford': [
+        {
+            'model': 'mustang',
+            'year': '2012'
+        }, {
+            'model': 'fusion',
+            'year': '2015'
+        }
+    ],
+}
