68cfb326c60a2467d1b3603765fad9ab341e631a
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2022-12-22 21:10:15 +0700

    Update JavascriptArrayHigherOrder.js

--- JavascriptArrayHigherOrder.js
+++ JavascriptArrayHigherOrder.js
@@ -1,6 +1,7 @@
 
 //Filter array of objects by sub array of values
 //filter movies base on value of genres
+//https://stackoverflow.com/questions/48997590/filter-array-of-objects-by-sub-array-of-values
 movies = [{
     'title': 'a',
     'genres': ['Romance', 'Comedy']
@@ -20,6 +21,7 @@ console.log(movies.filter(x => x.genres.some(g => filters.includes(g))))
 
 //********************************************************************************************************
 //Group an array of objects by keys
+//https://stackoverflow.com/questions/40774697/how-can-i-group-an-array-of-objects-by-key
 const cars = [
     {
         'make': 'audi',
