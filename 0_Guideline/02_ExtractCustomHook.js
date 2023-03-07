d41ff26d7071b7310ea73862a55fd7a593ca387e
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2023-03-08 02:11:28 +0700

    Create 02_ExtractCustomHook.js

---
+++ 0_Guideline/02_ExtractCustomHook.js
@@ -0,0 +1,32 @@
+/***********
+  Instead of using Container and Element for smart component (logic) and dumb component (UI)
+  We can extract the states and functions (logic) into a customHook
+  
+  1. Single Responsibility Principle
+  Break code into smaller components when possible
+  Whenever there is useState and useEffect, move it into a customHook if possible
+*/
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+export const useProducts = () => {
+  const [products, setProducts] = useState([])
+  
+  const fetchProduct = async () => {
+    const response = axios.get('url.com')
+    
+    if(response && response.data) setProducts(response.data)
+  }
+    
+  useEffect(() => {
+    fetchProduct()
+  }, [])
+  
+  const otherCalculation = () => {
+    console.log('Do something else')
+  }
+  
+  //return states and functions for the component
+  return {products, otherCalculation}
+  
+}
