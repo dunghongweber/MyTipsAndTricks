7219fea4d36ec20e4b9e71ac9c8d174952a2af0f
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2023-03-08 02:16:04 +0700

    Update 02_ExtractCustomHook.js

--- 0_Guideline/02_ExtractCustomHook.js
+++ 0_Guideline/02_ExtractCustomHook.js
@@ -5,6 +5,12 @@
   1. Single Responsibility Principle
   Break code into smaller components when possible
   Whenever there is useState and useEffect, move it into a customHook if possible
+  
+  2. Dependency Inversion Principle
+  create a onSubmit function at a parent component
+  pass it as a prop to a child component (Form)
+  so now Form can be used anywhere and we can create a bunch of parent components with different onSubmit for each usage
+  
 */
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
