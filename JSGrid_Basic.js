3ecdcb6ff3ae804e6275f62c4946af1699ecdae5
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2023-03-08 00:59:58 +0700

    Update JSGrid_Basic.js

--- JSGrid_Basic.js
+++ JSGrid_Basic.js
@@ -1,7 +1,31 @@
+//https://css-tricks.com/snippets/css/complete-guide-grid/
 import styled from styled-component
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: ...  ...;
-  grid-template-rows: ... ...;
+  grid-template-columns: 1fr 100px;
+  //using CSS REPEAT : https://developer.mozilla.org/en-US/docs/Web/CSS/repeat
+  grid-template-rows: repeat(6, 1fr);
+  
+  column-gap: 1rem;
+  row-gap: 1rem;
+  gap: 1rem;
+  
+  justify-items: start | end | center | stretch;
+  align-items: start | end | center | stretch;
+  // place-items sets both the align-items and justify-items properties in a single declaration.
+  place-items: center;
+  // alignment of the grid within the grid container.
+  justify-content: start | end | center | stretch | space-around | space-between | space-evenly;
+`
+
+/**
+  make grid columns same height
+*/
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`
+export const Children = style.div`
+  height: 100%;
 `
