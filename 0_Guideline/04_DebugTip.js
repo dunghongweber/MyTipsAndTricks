7226cb33a49e32be1ff0b1b8c84473228c92ebd2
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2023-03-08 04:50:37 +0700

    Create 04_DebugTip.js

---
+++ 0_Guideline/04_DebugTip.js
@@ -0,0 +1,5 @@
+//to log and check object with its variable name by
+console.log({ item, product, drink }); //computed property names
+
+//if objects have same properties, can use table to log
+console.table([age, name, gender]); //this is best for array of objects
