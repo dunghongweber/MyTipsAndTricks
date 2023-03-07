c5b4d21452920430015d1d45e23c8d3f4c84ba91
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2023-03-08 05:00:08 +0700

    Create 06_GroupArrayOfObjectsByKey_Reduce.js

---
+++ Javascript_Array/06_GroupArrayOfObjectsByKey_Reduce.js
@@ -0,0 +1,49 @@
+//Group an array of objects by keys
+//https://stackoverflow.com/questions/40774697/how-can-i-group-an-array-of-objects-by-key
+const cars = [
+    {
+        'make': 'audi',
+        'model': 'r8',
+        'year': '2012'
+    }, {
+        'make': 'audi',
+        'model': 'rs5',
+        'year': '2013'
+    }, {
+        'make': 'ford',
+        'model': 'mustang',
+        'year': '2012'
+    }, {
+        'make': 'ford',
+        'model': 'fusion',
+        'year': '2015'
+    }
+];
+
+const result = cars.reduce(function (r, a) {
+        r[a.make] = r[a.make] || [];
+        r[a.make].push(a);
+        return r;
+    }, {});
+//now result will be grouped as below
+const cars_result = {
+    'audi': [
+        {
+            'model': 'r8',
+            'year': '2012'
+        }, {
+            'model': 'rs5',
+            'year': '2013'
+        },
+    ],
+
+    'ford': [
+        {
+            'model': 'mustang',
+            'year': '2012'
+        }, {
+            'model': 'fusion',
+            'year': '2015'
+        }
+    ],
+}
