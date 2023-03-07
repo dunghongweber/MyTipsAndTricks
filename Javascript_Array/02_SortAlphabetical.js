292b3d6a18fc1932c8ad4f80bd4e17bc2a6929c8
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2023-03-08 04:53:15 +0700

    Create 02_SortAlphabetical.js

---
+++ Javascript_Array/02_SortAlphabetical.js
@@ -0,0 +1,13 @@
+//In our case if two elements are a and b we want to compare a.firstname and b.firstname
+users.sort(function(a, b){
+    if(a.firstname < b.firstname) { return -1; }
+    if(a.firstname > b.firstname) { return 1; }
+    return 0;
+})
+
+/**
+    OR using String.prototype.localeCompare()
+    read more about localeCompare here
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
+*/
+users.sort((a, b) => a.firstname.localeCompare(b.firstname))
