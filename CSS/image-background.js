1cfc29dac91f7a46d2e6fa9a3b3d0330575e67e9
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2023-03-08 19:28:08 +0700

    Update image-background.js

--- CSS/image-background.js
+++ CSS/image-background.js
@@ -1,11 +1,11 @@
 import styled from "styled-component"
 
 export const DivImg = styled.div`
-  background-image: url("photographer.jpg"); /* The image used */
-  background-color: #cccccc; /* Used if the image is unavailable */
-  height: 500px; /* You must set a specified height */
-  background-position: center; /* Center the image */
-  background-repeat: no-repeat; /* Do not repeat the image */
-  background-size: cover; /* Resize the background image to cover the entire container */
+  background-image: url("photographer.jpg"); // The image used
+  background-color: #cccccc; // Used if the image is unavailable
+  height: 500px; // You must set a specified height
+  background-position: center; // Center the image
+  background-repeat: no-repeat; // Do not repeat the image
+  background-size: cover; // Resize the background image to cover the entire container
 `
 
