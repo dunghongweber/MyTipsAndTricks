6cd70beed4126a6d999aad338c43fb19a5a2bdf0
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2023-03-08 19:42:44 +0700

    Create editSVGiconButton.js

---
+++ CSS/editSVGiconButton.js
@@ -0,0 +1,9 @@
+import styled from 'styled-component'
+import {Button as AntButton} from 'antd'
+
+export const MyButton = styled()`
+  svg {
+    stroke-width: 2rem; //thickness
+    stroke: black; //color
+  }
+`
