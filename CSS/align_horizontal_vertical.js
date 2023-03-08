945f77e59e0bb049de8e95bd2b327989789d9d9b
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2023-03-08 19:32:20 +0700

    Create align_horizontal_vertical.js

---
+++ CSS/align_horizontal_vertical.js
@@ -0,0 +1,20 @@
+import styled from "styled-component"
+
+export const EasyCenterAlign = styled.div`
+  text-align: center; //vertical
+  padding: 1.5rem 0; //horizontal
+`
+
+export const UsingFlexBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const CenterImage = styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+`
+
