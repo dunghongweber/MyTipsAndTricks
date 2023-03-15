d234a6d6e32108dc9199257a1d9a60ccc94963db
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2023-03-15 14:18:46 +0700

    Update 05_useMemo_useCallback.js

--- 0_Guideline/05_useMemo_useCallback.js
+++ 0_Guideline/05_useMemo_useCallback.js
@@ -11,3 +11,24 @@ const total = useMemo(() => {
 const sortedPeople = useMemo(() => {
   [...people].sort()
 }, [people])
+
+
+/***********
+  USECALLBACK, when to use:
+  
+  1. When you want to keep your callback function to being stale
+  2. When you want to retain the referential identity of the callback function
+*/
+//in Parent Component
+const sortFunc = useCallback ((a,b) => ..., [])  //prevent recreate the function everytime Parent Component re-renders
+
+<NameList names={names} sortFunc={sortFunc} />
+//in Child Component
+ const NameList = ({names, sortFunc}) => {
+   //sortFunc won't be recreated every time Parent re-render
+    const sortedNames = useMemo(() => {
+       [...names].sort(sortFunc)
+    },[names, sortFunc])
+    
+    return (<div>...</div>)
+ }
