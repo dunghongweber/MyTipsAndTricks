e714d53275135a2f154222ba1c0bf0c5f06b0436
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2023-04-20 22:40:20 +0700

    Update 01_oneLinersFunctions.js

--- helperFunctions/01_oneLinersFunctions.js
+++ helperFunctions/01_oneLinersFunctions.js
@@ -55,3 +55,16 @@ insert(items, 2, 3); // [1, 2, 3, 4, 5]
 // format a number with commas as thousands separators
 const numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 
+//In our case if two elements (objects) are a and b we want to compare a.firstname and b.firstname
+users.sort(function(a, b){
+    if(a.firstname < b.firstname) { return -1; }
+    if(a.firstname > b.firstname) { return 1; }
+    return 0;
+})
+
+/**
+    OR using String.prototype.localeCompare()
+    read more about localeCompare here
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
+*/
+users.sort((a, b) => a.firstname.localeCompare(b.firstname))
