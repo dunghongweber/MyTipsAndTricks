97da4cc361f6e54316feb793911f6fa17ea50894
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2023-07-21 01:03:30 +0700

    Update Basic.tsx

--- ReactTS/Basic.tsx
+++ ReactTS/Basic.tsx
@@ -1,20 +1,23 @@
-//Interface and Props type
-//create them in an Interface folder for each main component
-//then export and share among the children
-export interface IProject {
+/*
+  Interface and Props type
+  create them in an Interface folder for each main component
+  then export and share among the children
+*/
+export interface IProject { //naming interface with I infront
+  name: string
+  link: string
+  description: string
+}
+export type PropCard = { //naming props with Props
   name: string
   link: string
   description: string
 }
 
-export type PropCard = {
-  name: string
-  link: string
-  description: string
-}
-
-//USE Interface and Props
-//import and declare it in ReactTS
+/*
+  USE Interface and Props: import and declare it in ReactTS
+  https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/default_props
+*/
 import React from 'react'
 import { PropCard } from '../interfaces/interfaces'
 
@@ -25,3 +28,22 @@ const ComponentCard: React.FC<PropCard> = ({ name, link, description }) => {
   )
 }
 export default ComponentCard
+
+/*
+  React CHILDREN in props type
+  https://blog.logrocket.com/using-react-children-prop-with-typescript/
+  https://www.carlrippon.com/react-children-with-typescript/
+*/
+import React from 'react'
+type Props = {
+  children: JSX.Element
+}
+
+const LayoutLandingPage: React.FC<Props> = ({ children }) => {
+  return (
+      <div style={{ marginTop: '3.5rem' }}>
+        {children}
+      </div>
+  )
+}
+export default LayoutLandingPage
